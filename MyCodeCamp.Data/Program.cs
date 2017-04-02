ea8f53a62d84ae37f32a8bf200befa6bb5ec90cc
Cleverson A S Dias <cleversonasdias@hotmail.com>
2017-04-02 10:30:02 -0300

    Preparação do projeto Data e criado as migracions

---
+++ MyCodeCamp.Data/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace MyCodeCamp.Data
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {}
+    }
+}
---
+++ MyCodeCamp.Data/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyCodeCamp.Data.Data;
+using MyCodeCamp.Data.Entities;
+
+namespace MyCodeCamp.Data
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+           .SetBasePath(env.ContentRootPath)
+           .AddJsonFile("appsettings.json");
+
+            _config = builder.Build();
+        }
+
+        IConfigurationRoot _config;
+
+        public IConfigurationRoot Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSingleton(_config);
+            services.AddDbContext<CampContext>(ServiceLifetime.Scoped)
+              .AddIdentity<CampUser, IdentityRole>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure() { }
+    }
+}
